abfed763342f691f1d58d7d624cd14dbfd536827
santiagogomez102 <santiago.gomez102@outlook.com>
2023-12-02 14:05:31 -0500

    Gameplay
    More obstacles added.
    Air resistance added (through code and Unity options)
    Fog added

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -6,10 +6,10 @@ public class PlayerMovement : MonoBehaviour{
     void FixedUpdate(){
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
         if(Input.GetKey("d")){
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if(Input.GetKey("a")){
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
