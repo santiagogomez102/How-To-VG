f50c0e433fcf27a14bd3d35791d17b51daab093d
santiagogomez102 <santiago.gomez102@outlook.com>
2023-12-01 22:34:52 -0500

    ForceByTime
    With "Time.deltaTime", force depends on actual time, not frames

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -5,12 +5,12 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     void Start()
     {
-        rb.AddForce(0, 200, 500);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(0, 0, 200);
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
     }
 }
