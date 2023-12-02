2381e9bdf0bb3c980ffa0a13c4bc16316710b7b7
santiagogomez102 <santiago.gomez102@outlook.com>
2023-12-02 00:06:23 -0500

    "sidewaysForce" added
    Controls how to move player with keyboard

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour{
     public Rigidbody rb;
-    public float forwardForce = 200;
+    public float forwardForce = 200f;
+    public float sidewaysForce = 200f;
     void FixedUpdate(){
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
         if(Input.GetKey("d")){
-            rb.AddForce(500 * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey("a")){
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
         }
     }
 }
