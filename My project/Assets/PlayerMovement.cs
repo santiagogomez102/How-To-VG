6ec7afc0cbb022abc8e1ac6d158013eae09c292f
santiagogomez102 <santiago.gomez102@outlook.com>
2023-12-01 23:21:36 -0500

    FrictionAdded&ForceAsVariable
    Friction was added to the ground, and the force is now a variable

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Time.deltaTime to depend on time, not frames
     public Rigidbody rb;
+    public float forwardForce = 2000;
+
     void Start()
     {
         
     }
-
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
     }
 }
