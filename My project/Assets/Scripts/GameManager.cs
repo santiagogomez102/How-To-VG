171da9b567a581c6b55c6f4275d80e92dbdd8fec
santiagogomez102 <santiago.gomez102@outlook.com>
2023-12-02 18:15:17 -0500

    Animations and other levels
    Using other Text spaces and animations, a change of level was made

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour{
     bool gameHasEnded = false;
     public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    public void CompleteLevel () {
+        completeLevelUI.SetActive(true);
+    }
     public void EndGame (){
         if(gameHasEnded == false){
             gameHasEnded = true;
