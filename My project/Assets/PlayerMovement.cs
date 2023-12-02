4d40a32984a0c30b2f7589a1ba2d63e3b1f36015
santiagogomez102 <santiago.gomez102@outlook.com>
2023-12-01 22:06:55 -0500

    PlayerWithGravity
    Gravity programmed to player

---
+++ My project/Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    void Start()
+    {
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
